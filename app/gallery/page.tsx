e38cbae81d428b5ef29b1f3c9828a750c8f83dd5
Ritesh Birthal <riteshbirthal@gmail.com>
2025-11-28 01:13:46 +0530

    Initial Commit

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
 
-export default function Home() {
+
+export default function Gallery() {
   return (
     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      Home Page
+      Gallery Page
     </main>
   );
 }
