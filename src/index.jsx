66480ce95fa3290ae860171f613b7e2e2c727853
Ritesh Birthal <riteshbirthal@gmail.com>
2025-12-19 15:38:24 +0530

    Synchronized react app

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom'
 
-const basename = 'https://riteshbirthal.github.io/pharma-store/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={basename}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
